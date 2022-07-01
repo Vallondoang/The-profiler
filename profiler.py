84526a02cc9661a6037ae76dc4335ce0afacda42
Vallondoang <106737402+Vallondoang@users.noreply.github.com>
2022-07-01 17:39:08 +0700

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -11,9 +11,20 @@ def reset():
     inputcommand = input()
     if inputcommand == "1":
         print("insert keyword")
-        print("dont forget to add quotation mark in the sentences")
-        query = input()
- 
+        print("dont forget to add quotation mark in the sentences for more accurate search")
+        keyword = input()
+        print("insert spesific site")
+        print("leave blank if you dont use it")
+        insiteword = input()
+        insite = "site:" + insiteword
+        print("insert what are you looking for in the site")
+        print("leave blank if you dont use it")
+        inurlword = input()
+        inurl = "inurl:" + inurlword
+        query = keyword + " " + insite + " " + inurl
+
+        print(query)
+
         for j in search(query, tld="co.in", num=10, stop=10, pause=2):
             print(j)
         
